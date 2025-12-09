37f086f009eb014c34ec1e4e6c28db4d9f687549
yeop6033 <117691942+yeop6033@users.noreply.github.com>
2025-12-09 22:01:43 +0900

    [Silver III] Title: 소수 구하기, Time: 312 ms, Memory: 42212 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,68 +1,19 @@
 import sys
+import math
 
-input = sys.stdin.readline
+M, N = map(int, sys.stdin.readline().split())
 
-test_num = list(map(int, input().split()))
+# 소수 체크 리스트
+prime = [True] * (N + 1)
+prime[0] = prime[1] = False  # 0,1는 소수 아님
 
-primes = []
+# 에라토스테네스의 체
+for i in range(2, int(math.sqrt(N)) + 1):
+    if prime[i]:
+        for j in range(i * i, N + 1, i):
+            prime[j] = False
 
-for i in range(test_num[0],test_num[1]+1):   
-    x = i
-
-    
-    if x < 2:
-        continue
-
-    # 작은 소수 빠른 처리
-    if x == 2 or x == 3 or x == 5:
-        primes.append(x)
-        continue
-
-    # 2,3,5의 배수는 소수 아님
-    if x % 2 == 0 or x % 3 == 0 or x % 5 == 0:
-        continue
-
-    # -------- 여기서부터 Miller–Rabin --------
-    d = x - 1
-    s = 0
-    
-    while d % 2 == 0:
-        d //= 2
-        s += 1
-
-    ok = True 
-
-    
-    for a in (2, 7, 61):
-        if a >= x:
-            continue
-
-        
-        y = pow(a, d, x)
-
-        
-        if y == 1 or y == x - 1:
-            continue
-
-        passed = False
-       
-        for _ in range(s - 1):
-            y = (y * y) % x
-            if y == x - 1:
-                passed = True
-                break
-
-        
-        if not passed:
-            ok = False
-            break
-
-
-    if ok:
-        primes.append(x)
-
-
-for p in primes:
-    print(p)
-
-
+# 출력
+for i in range(M, N + 1):
+    if prime[i]:
+        print(i)
